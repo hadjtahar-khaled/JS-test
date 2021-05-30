a89bf0b0aa7d9c34f5ca7752076debb999cd6682
hadjtahar-khaled <50873499+hadjtahar-khaled@users.noreply.github.com>
2021-05-30 01:13:49 +0100

    Create fonction4.js

---
+++ fonction4.js
@@ -0,0 +1,14 @@
+// 4)-function called showNumbers 
+function ShowNumbers(a){
+	
+	for (let i=0; i<= a ; i++){
+		if(i%2){
+			console.log(i +" is ODD")
+		}else{
+			console.log(i + " is EVEN")
+		}
+	}
+}
+//exemple d'excution 
+
+ShowNumbers(10)
